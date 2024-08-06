ed94d56effa125f0298087950e95dbac5a5039c4
akira <ekereymen451+645675476345635573454309@gmail.com>
2024-08-06 10:26:08 +0300

    Delete xyz/chudvvick/akeno/utils/ColorUtil.java

--- xyz/chudvvick/akeno/utils/ColorUtil.java
+++
@@ -1,9 +0,0 @@
-package xyz.chudvvick.akeno.utils;
-
-import net.md_5.bungee.api.ChatColor;
-
-public class ColorUtil {
-   public static String fixColor(String x) {
-      return ChatColor.translateAlternateColorCodes('&', x);
-   }
-}
